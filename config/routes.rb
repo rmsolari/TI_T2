da41bb7e9b5809ea9cff201b4a299cf6b913bead
rmsolari <rmsolari@uc.cl>
2018-04-03 12:21:08 -0300

    a

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   put '/news/:id/', to: 'entries#update_put'
-  patch 'news/:id', to: 'entries#update_patch'
+  patch '/news/:id', to: 'entries#update_patch'
   resources :entries, path: 'news' do
     put '/comments/:id/', to: 'comments#update_put'
-    patch 'comments/:id', to: 'comments#update_patch'
+    patch '/comments/:id', to: 'comments#update_patch'
     resources :comments
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
